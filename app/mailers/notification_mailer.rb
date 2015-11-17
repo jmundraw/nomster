d8344603a79b17f75287e2051ee5a3766152e5c5
Jackie Mundrawala <jmundrawala@gmail.com>
2015-11-17 02:06:50 +0000

    Fix email preview

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	mail(to: "jmundrawala@gmail.com", subject: "A comment has been added to your place." )
+  	mail(to: "testforweb7@gmail.com", subject: "A comment has been added to your place." )
   end
 end
 
